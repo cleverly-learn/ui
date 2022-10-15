a6a03048b940bf2834f3ed59748c965dbeaf9017
vladhuk <vladhookovskiy@gmail.com>
2022-10-16 02:57:42 +0300

    fix skeleton

--- src/components/Dashboard/components/Courses/components/ListSkeleton.tsx
+++ src/components/Dashboard/components/Courses/components/ListSkeleton.tsx
@@ -1,17 +1,16 @@
-import { Grid, Grow, Skeleton } from '@mui/material';
+import { Grid, Skeleton } from '@mui/material';
 import React, { FC } from 'react';
 
 export const ListSkeleton: FC = () => (
   <Grid container spacing={2.5}>
-    {Array.from(Array(6)).map((index: number) => (
-      <Grow key={index} timeout={50 * index} in>
-        <Grid item md={4}>
-          <Skeleton
-            variant="rectangular"
-            sx={{ borderRadius: '10px', height: 112 }}
-          />
-        </Grid>
-      </Grow>
+    {Array.from(Array(6)).map((value, index) => (
+      // eslint-disable-next-line react/no-array-index-key
+      <Grid item key={index} md={4}>
+        <Skeleton
+          variant="rectangular"
+          sx={{ borderRadius: '10px', height: 112 }}
+        />
+      </Grid>
     ))}
   </Grid>
 );
